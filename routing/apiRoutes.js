e6f26283c88b70901ab27c8974bcac062ee079a4
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2018-05-27 17:50:00 -0400

    updated file paths to support Heroku deployment

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -10,6 +10,7 @@ module.exports = function (app) {
   //* A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
 
   app.post('/api/friends', function (req, res) {
+    console.log(req.body);
     //put in the compatiblity logic
     //6. Determine the user's most compatible friend using the following as a guide:
 
--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 //bring in npms and call express
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 //make the public folder a static folder
-app.use("/public",express.static("./app/public"));
+app.use("/public",express.static("./public"));
 //first public here is virtual path, second "" is that actual path to the folder you want to create the virtual path
 app.use(express.static("public"));
-//
-require("./app/routing/apiRoutes")(app);
-
-require("./app/routing/htmlRoutes.js")(app);
-var bodyParser = require("body-parser");
-
-var PORT = 5000;
 
 //from previous exercise 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//
+require("./routing/apiRoutes")(app);
+
+require("./routing/htmlRoutes.js")(app);
+
+
+var PORT = 5000;
+
+
 
 
 
